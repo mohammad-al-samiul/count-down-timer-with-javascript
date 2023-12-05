fd2bb9a36f63569d03a9d026a680361a801b8ba9
Noob-H4cker <alsamiul123@gmail.com>
2023-12-05 11:39:21 +0600

    start button eventlister add

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hi");
+(function () {
+  let hour = document.querySelector(".hour");
+  
+  let min = document.querySelector(".minute");
+  let sec = document.querySelector(".second");
+
+  let startBtn = document.querySelector(".start");
+  let stopBtn = document.querySelector(".stop");
+  let resetBtn = document.querySelector(".reset");
+
+  startBtn.addEventListener("click", function (){
+    console.log(hour.value);
+  })
+
+
+
+
+})();

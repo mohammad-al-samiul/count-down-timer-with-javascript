3c73f574971639b0da0aee269c02cbb4e6ed21fc
Noob-H4cker <alsamiul123@gmail.com>
2023-12-11 19:21:34 +0600

    hour logic written

--- script.js
+++ script.js
@@ -34,11 +34,15 @@
       min.value = "";
       sec.value = "";
       stopInterval();
-    } else if (sec != 0) {
+    } else if (sec.value != 0) {
       sec.value = `${sec.value <= 10 ? "0" : ""}${sec.value - 1}`;
     }else if(min.value != 0 && sec.value == 0){
       sec.value = 59;
       min.value = `${min.value <= 10 ? "0" : ""}${min.value - 1}`
+    }else if(hour.value != 0 && min.value == 0){
+      
+      min.value = 60;
+      hour.value = `${hour.value <= 10 ? "0" : ""}${hour.value - 1}`
     }
   }
 

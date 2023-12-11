f3c8f245164b3c231a9fc1c911f24c281e928b7e
Noob-H4cker <alsamiul123@gmail.com>
2023-12-11 19:09:55 +0600

    minute timer added

--- script.js
+++ script.js
@@ -34,8 +34,11 @@
       min.value = "";
       sec.value = "";
       stopInterval();
-    } else if (sec.value != 0) {
+    } else if (sec != 0) {
       sec.value = `${sec.value <= 10 ? "0" : ""}${sec.value - 1}`;
+    }else if(min.value != 0 && sec.value == 0){
+      sec.value = 59;
+      min.value = `${min.value <= 10 ? "0" : ""}${min.value - 1}`
     }
   }
 
